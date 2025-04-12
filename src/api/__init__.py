ce0b587cccee2193b3750d685d7f6881ccf82a0b
Arthicsu <ronivl@yandex.ru>
2025-04-12 17:18:25 +0300

    bugfixes

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from .routers import display_router
+from .routers import fashion_router
 
 router = APIRouter()
 
-router.include_router(display_router.router)
+router.include_router(fashion_router.router)
--- train.py
+++ train.py
@@ -1,35 +1,62 @@
-import pandas as pd
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer #
-import joblib
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 
-data = pd.read_excel("/content/data.xlsx")
 
-X = data.drop(["Defect_level", "Quality_score"], axis=1)
-y = data[["Defect_level", "Quality_score"]]
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-numeric_cols = ["Length", "Width", "Quality", "Size_metric"]
-categorical_cols = ["Type"]
 
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', StandardScaler(), numeric_cols),
-        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_cols)
-    ])
+print(f"x_train shape: {x_train.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"x_test shape: {x_test.shape}")
+print(f"y_test shape: {y_test.shape}")
 
-X_processed = preprocessor.fit_transform(X)
 
-joblib.dump(preprocessor, "/content/preprocessor.pkl")
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=(X_processed.shape[1],)),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(2)
-])
 
-model.compile(optimizer='adam', loss='mse')
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    idx = random.randint(0, len(x_train))
+    plt.imshow(x_train[idx], cmap='gray')
+    plt.title(f'{class_names[y_train[idx]]} ({y_train[idx]})')
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
 
-model.fit(X_processed, y, epochs=50, batch_size=8, validation_split=0.2) # батч
+# Предобработка данных
+# Нормализация и преобразование в float32
+x_train = x_train.reshape(60000, 784).astype('float32') / 255
+x_test = x_test.reshape(10000, 784).astype('float32') / 255
 
-model.save("/content/display_predictor.keras")
+# One-hot encoding для меток
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model = Sequential()
+model.add(Dense(512, input_shape=(784,), activation="relu"))
+model.add(Dropout(0.2))
+
+model.add(Dense(512, activation="relu"))# Добавление второго скрытого слоя
+model.add(Dropout(0.2))
+
+model.add(Dense(10, activation="softmax"))# Выходной полносвязный слой, 10 нейронов (по количеству классов)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+train = model.fit(x_train, y_train, batch_size=1024, epochs=40, validation_split=0.2, verbose=1)
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1)
+
+scores = model.evaluate(x_test, y_test, verbose=1)
+print(f'Значение функции потерь (loss) на тестовых данных: {scores[0]}')
+print(f'Доля верных ответов на тестовых данных, в процентах (accuracy): {round(scores[1] * 100, 4)}')
+
+model.save("/content/fashion_model.keras")
