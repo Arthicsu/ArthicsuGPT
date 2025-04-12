acfe7158839ac3256bfc3f86a1e2f84f7a93926e
Arthicsu <ronivl@yandex.ru>
2025-04-12 16:58:59 +0300

    fashion_mnist

--- src/api/routers/display_router.py
+++ src/api/routers/display_router.py
@@ -1,59 +1,56 @@
-from fastapi import APIRouter, Request, Form
+from fastapi import APIRouter, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 import tensorflow as tf
-import pandas as pd
-import joblib
+import numpy as np
+import uuid
+
 
 router = APIRouter()
-
 templates = Jinja2Templates(directory="templates")
 
-network = tf.keras.models.load_model('src/models/display_predictor.keras')
-preprocessor = joblib.load("src/models/preprocessor.pkl")
+model = tf.keras.models.load_model('src/models/fashion_model_v3.keras')
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 
 @router.get("/", response_class=HTMLResponse)
 async def show_form(request: Request):
-    return templates.TemplateResponse("display_form.html", {"request": request, "title": "Оценка дисплея"})
+    return templates.TemplateResponse("display_form.html", {"request": request, "title": "Распознавание одежды"})
+
 
 @router.post("/predict", response_class=HTMLResponse)
 async def predict_display(
-    request: Request,
-    length: float = Form(...),
-    width: float = Form(...),
-    quality: float = Form(...),
-    display_type: str = Form(...),
+        request: Request,
+        file: UploadFile = File(...)
 ):
     try:
-        size_metric = length * width
+        temp_filename = f"assets/temp/{uuid.uuid4()}.png"
+        temp_filepath = f"static/{temp_filename}"
 
-        input_dict = {
-            "Length": length,
-            "Width": width,
-            "Quality": quality,
-            "Type": display_type,
-            "Size_metric": size_metric,
-        }
+        with open(temp_filepath, "wb") as buffer:
+            buffer.write(await file.read())
 
-        new_data = pd.DataFrame([input_dict])
-        processed = preprocessor.transform(new_data)
-        prediction = network.predict(processed, verbose=0)[0]
+        img = tf.keras.preprocessing.image.load_img(
+            temp_filepath,
+            target_size=(28, 28),
+            color_mode='grayscale'
+        )
 
-        defect_level = float(prediction[0])
-        quality_score = float(prediction[1])
+        x = tf.keras.preprocessing.image.img_to_array(img)
+        x = x.reshape(1, 784).astype('float32')
+        x = (255 - x) / 255
+
+        pred = model.predict(x)
+        pred_class = np.argmax(pred)
 
         return templates.TemplateResponse(
-            "display_result.html",
-            {
+            "display_result.html",{
                 "request": request,
                 "title": "Результаты",
-                "length": length,
-                "width": width,
-                "quality": quality,
-                "Type": display_type,
-                "defect_level": round(defect_level, 1),
-                "quality_score": round(quality_score, 1),
+                "predicted_class": class_names[pred_class],
+                "ver": round(float(np.max(pred[0])), 4),
+                "image_url": f"/static/{temp_filename}"
             },
         )
     except Exception as e:
